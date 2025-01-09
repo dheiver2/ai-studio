eb694a780bcca3c066c4a0b064aad7871db22c24
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-08 23:58:23 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  distDir: 'build'  // Especifica o diretório de build
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
